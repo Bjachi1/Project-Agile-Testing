33e0ee4e110f1df7bd1f691d50c6f9ae35f2d4ad
Bjachi1 <55455309+Bjachi1@users.noreply.github.com>
2020-10-08 15:22:17 +0200

    Switch from .net WPF to ASP.net

---
+++ Website Project Agile/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Website_Project_Agile.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
